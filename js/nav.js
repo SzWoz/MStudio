9682b28396c7c2479aa093a4e3a103d9150b8ec0
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2021-12-05 18:28:02 +0100

    navbar animations
    added animations to navbar

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,27 @@
-let prevScroll = window.scrollY;
-console.log("seg")
-let content = document.querySelector(".content");
-let nav = document.querySelector("nav");
-content.onscroll = () => {
+// let prevScroll = window.scrollY;
+// console.log("seg")
+// let content = document.querySelector(".content");
+// let nav = document.querySelector("nav");
+// // content.onscroll = () => {
 
-    let currScroll = content.scrollTop;
-    if (currScroll > prevScroll) {
-        nav.classList.add("hidden")
+// //     let currScroll = content.scrollTop;
+// //     if (currScroll > prevScroll) {
+// //         nav.classList.add("hidden")
+// //     } else {
+// //         nav.classList.remove("hidden");
+// //     }
+// //     prevScroll = currScroll;
+// // }
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener('click', () => {
+    if (!hamburger.classList.contains('cross')) {
+        hamburger.classList.add('cross');
+        menu.classList.remove('disabled');
     } else {
-        nav.classList.remove("hidden");
+        hamburger.classList.remove('cross');
+        menu.classList.add('disabled');
     }
-    prevScroll = currScroll;
-}
+})
