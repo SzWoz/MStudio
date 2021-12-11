f621229f5bf0d8d782599d07b9f3e77d535eeec1
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2021-12-11 01:03:28 +0100

    changes in menu hovers

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+const site = document.querySelector(".site");
+site.style.transition = "filter 2s ease";
 
 hamburger.addEventListener('click', () => {
     if (!hamburger.classList.contains('cross')) {
         hamburger.classList.add('cross');
         menu.classList.remove('disabled');
+        site.style.filter = "grayscale(100%)";
     } else {
         hamburger.classList.remove('cross');
         menu.classList.add('disabled');
+        site.style.filter = null;
     }
 })
