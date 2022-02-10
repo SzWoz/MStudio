2ad153a741750ebd83104d4f5080f0e5e68583df
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2022-02-10 19:11:41 +0100

    repairing interval function

--- js/offer.js
+++ js/offer.js
@@ -14,6 +14,8 @@ let slideWidth = 500;
 let firstSlide = slide[0].cloneNode(true);
 let lastSlide = slide[slide.length - 1].cloneNode(true);
 
+let animationFinished = true;
+
 firstSlide.id = 'first-slide';
 lastSlide.id = 'last-slide';
 
@@ -26,60 +28,61 @@ slider.prepend(lastSlide);
 slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
 
 
+let timer = setInterval(intervalFunc, interval);
 
 
-setInterval(() => {
+function intervalFunc() {
     slidePos++;
     slider.style.transition = "0.7s";
 
     slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
-}, interval);
+}
 
 
 next.addEventListener("click", () => {
-    slide = document.querySelectorAll(".slide");
+    if (animationFinished != false) {
+        slide = document.querySelectorAll(".slide");
 
-    slidePos++;
-    slider.style.transition = "0.7s";
+        slidePos++;
+        slider.style.transition = "0.7s";
 
-    slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
-    console.log(slide[slidePos])
-
-    if (slidePos > 10) slidePos = 10;
-    console.log(slidePos)
-
+        slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
+        console.log(slide[slidePos])
+        clearInterval(timer);
+        timer = setInterval(intervalFunc, interval);
+    }
+    animationFinished = false;
 
 });
 prev.addEventListener("click", () => {
-    slide = document.querySelectorAll(".slide");
+    if (animationFinished != false) {
 
-    slidePos--;
-    slider.style.transition = "0.7s";
+        slide = document.querySelectorAll(".slide");
 
-    slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
+        slidePos--;
+        slider.style.transition = "0.7s";
 
-    console.log(slidePos)
+        slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
 
-    console.log(slideWidth * slidePos)
-    if (slidePos < 0) slidePos = 0;
+        clearInterval(timer);
+        timer = setInterval(intervalFunc, interval);
+    }
+    animationFinished = false;
 
 });
 
 slider.addEventListener("transitionend", () => {
+    animationFinished = true;
     slide = document.querySelectorAll(".slide");
     if (slide[slidePos].id === firstSlide.id) {
         slidePos = 1;
         slider.style.transition = "none"
         slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
-        console.log("jeeeac")
     } else if (slide[slidePos].id === lastSlide.id) {
         slider.style.transition = "none"
 
         slidePos = slide.length - 2;
 
         slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
-
-        console.log(slideWidth * slidePos)
     }
 });
-
