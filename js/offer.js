3dd8d2c25803b36f40c9a55acfaad0347a97ee66
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2022-02-10 17:18:14 +0100

    slider scripting finished

--- js/offer.js
+++ js/offer.js
@@ -3,6 +3,7 @@ const slider = document.querySelector(".slides");
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 
+const interval = 4000;
 
 let slide = document.querySelectorAll(".slide");
 
@@ -27,6 +28,13 @@ slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
 
 
 
+setInterval(() => {
+    slidePos++;
+    slider.style.transition = "0.7s";
+
+    slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
+}, interval);
+
 
 next.addEventListener("click", () => {
     slide = document.querySelectorAll(".slide");
@@ -37,6 +45,10 @@ next.addEventListener("click", () => {
     slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
     console.log(slide[slidePos])
 
+    if (slidePos > 10) slidePos = 10;
+    console.log(slidePos)
+
+
 });
 prev.addEventListener("click", () => {
     slide = document.querySelectorAll(".slide");
@@ -44,23 +56,30 @@ prev.addEventListener("click", () => {
     slidePos--;
     slider.style.transition = "0.7s";
 
-    slider.style.transform = `translateX(${slideWidth * slidePos}px)`;
+    slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
 
     console.log(slidePos)
 
     console.log(slideWidth * slidePos)
-
+    if (slidePos < 0) slidePos = 0;
 
 });
 
 slider.addEventListener("transitionend", () => {
     slide = document.querySelectorAll(".slide");
-    if (slide[slidePos].id == firstSlide.id) {
+    if (slide[slidePos].id === firstSlide.id) {
         slidePos = 1;
         slider.style.transition = "none"
         slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
         console.log("jeeeac")
+    } else if (slide[slidePos].id === lastSlide.id) {
+        slider.style.transition = "none"
+
+        slidePos = slide.length - 2;
+
+        slider.style.transform = `translateX(${-slideWidth * slidePos}px)`;
+
+        console.log(slideWidth * slidePos)
     }
-    console.log("gowmo")
 });
 
